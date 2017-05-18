402244ce430b56dec233b38373deb9953de71584
Danielle Penny <danielle.penny@doordash.com>
2017-05-18 14:27:55 -0700

    clean tests

--- ddl/api_tests/test_food_item.py
+++ ddl/api_tests/test_food_item.py
@@ -11,19 +11,12 @@ from rest_framework.test import APITestCase
 class TestStuff(unittest.TestCase):
   @pytest.fixture(autouse=True, scope='session')
   def setUp(self):
-    # Every test needs a client.
     self.client = Client()
-    pytestmark = pytest.mark.usefixtures("initdb")
     Consumer.objects.create(first_name="foo", last_name="foo", email="foofoo", phone_number="foo")
     self.factory = RequestFactory()
+
   @pytest.fixture(autouse=True, scope='session')
   def test_get_existing_customer(self):
-    pytestmark = pytest.mark.usefixtures("initdb")
-
-    self.client = Client()
-    c = Consumer(first_name="foo", last_name="foo", email="foo", phone_number="foo")
-    c.save()
-    print Consumer.objects.first()
     response = self.client.get('/ddl/customers/1/')
     self.assertEqual(response.status_code, 200)
 
